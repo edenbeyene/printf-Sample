514ca4f2723093562cf5bd396fa3d7b8873ee32b
Your Name <you@example.com>
2022-07-11 00:36:21 -0700

    number of character printf

--- printf.c
+++ printf.c
@@ -1,185 +1,90 @@
 #include "main.h"
 
+void cleanup(va_list args, buffer_t *output);
+int run_printf(const char *format, va_list args, buffer_t *output);
+int _printf(const char *format, ...);
+
 /**
- * _printf - Simulates the printf function
- * @format: Format to print to the console
+ * cleanup - Peforms cleanup operations for _printf.
+ * @args: A va_list of arguments provided to _printf.
+ * @output: A buffer_t struct.
+ */
+void cleanup(va_list args, buffer_t *output)
+{
+	va_end(args);
+	write(1, output->start, output->len);
+	free_buffer(output);
+}
+
+/**
+ * run_printf - Reads through the format string for _printf.
+ * @format: Character string to print - may contain directives.
+ * @output: A buffer_t struct containing a buffer.
+ * @args: A va_list of arguments.
  *
- * Description: Function that simulate the printf function
- * for the following conversion specifiers:
- *    - c:    Chracter
- *    - s/S:  Strings. 's' prints the same string.
- *            'S' prints the string, changing the non-printable characters
- *            to '\x??' format, where '??' corresponds to a hexadecimal number
- *    - d/i:  Integers
- *    - o:    Octal numbers
- *    - x/X:  Hexadecimal numbers
- *    - b:    Binary numbers
- *    - u:    Unsigned numbers
- *    - %:    '%' char
- *    - r:    Reverse string
- *    - R:    Root13 of the string
- * The function considerate the next arguments, checking the usability for the
- * conversion specifier used:
- * (+)      Adds a '+' sign to positive integers
- * ( )      Adds a ' ' to the start of the number
- * (#)      Adds '0' or '0x' to the start of the octal or hexadecimal number
- * (l)      Converts the number to a 'long' type
- * (h)      Converts the number to a 'short' type
- * (width)  Add spaces(?) to the printing format
- * (preci)  Print the number with the corresponding precision
- * (0)      Add '0' to the spaces created with 'width'
- * (-)      Justify the string to the left
+ * Return: The number of characters stored to output.
+ */
+int run_printf(const char *format, va_list args, buffer_t *output)
+{
+	int i, wid, prec, ret = 0;
+	char tmp;
+	unsigned char flags, len;
+	unsigned int (*f)(va_list, buffer_t *,
+			unsigned char, int, int, unsigned char);
+
+	for (i = 0; *(format + i); i++)
+	{
+		len = 0;
+		if (*(format + i) == '%')
+		{
+			tmp = 0;
+			flags = handle_flags(format + i + 1, &tmp);
+			wid = handle_width(args, format + i + tmp + 1, &tmp);
+			prec = handle_precision(args, format + i + tmp + 1,
+					&tmp);
+			len = handle_length(format + i + tmp + 1, &tmp);
+
+			f = handle_specifiers(format + i + tmp + 1);
+			if (f != NULL)
+			{
+				i += tmp + 1;
+				ret += f(args, output, flags, wid, prec, len);
+				continue;
+			}
+			else if (*(format + i + tmp + 1) == '\0')
+			{
+				ret = -1;
+				break;
+			}
+		}
+		ret += _memcpy(output, (format + i), 1);
+		i += (len != 0) ? 1 : 0;
+	}
+	cleanup(args, output);
+	return (ret);
+}
+
+/**
+ * _printf - Outputs a formatted string.
+ * @format: Character string to print - may contain directives.
  *
- * Return: Number of characters printed
- * (excluding the null byte used to end output to strings)
+ * Return: The number of characters printed.
  */
 int _printf(const char *format, ...)
 {
-	va_list list;
-	char *buffer, *str;
-	int i = 0, i_buffer = 0, j_spec, lenstr, stock = 0;
+	buffer_t *output;
+	va_list args;
+	int ret;
 
-	buffer = malloc(1024);
-	if (buffer == NULL)
+	if (format == NULL)
 		return (-1);
-	va_start(list, format);
-	while (format && format[i])
-	{
-		if (format[i] == '%')
-		{
-			if (check_specs(format + i, &j_spec) == 1
-			    && check_format(format + i, j_spec))
-			{
-				str = generate_malloc(format + i, j_spec, list, &lenstr);
-				lenstr = format[i + j_spec - 1] == 'c' ? lenstr : _strlen(str);
-				if (str == NULL)
-					return (free_buffer(buffer, i_buffer));
-				_memcpy(buffer, str, &i_buffer, &stock, lenstr), free(str);
-				i += j_spec;
-				continue;
-			}
-			if (check_specs(format + i, &j_spec) == -1)
-				return (free_buffer(buffer, i_buffer));
-			if (format[i + 1] == 'h' || format[i + 1] == 'l')
-				_memcpy(buffer, format + i, &i_buffer, &stock, 1), j_spec = 2;
-			else
-				_memcpy(buffer, format + i, &i_buffer, &stock, j_spec);
-			i += j_spec;
-			continue;
-		}
-		_memcpy(buffer, format + i, &i_buffer, &stock, 1), i++;
-	}
-	va_end(list), i_buffer = write(1, buffer, i_buffer), free(buffer);
-	return (i == 0 && !format ? -1 : stock + i_buffer);
-}
-
-/**
- * check_specs - Check if a string has a conversion specifier
- * @s: String where to check the conversion specifier
- * @p: Pointer to the variable where the length to consider is saved
- * Return: -1 if ends the string
- * 0 if does not find a conversion specifier but does not end the format
- * 1 if find a conversition specifier
- */
-int check_specs(const char *s, int *p)
-{
-	char *specs = "%csSdioxXburRp";
-	char *flags = " 0-+#123456789.lh*";
-	int i = 0, j = 0, len_flags = _strlen(flags);
-
-	while (s[i])
-	{
-		j = 0;
-		while (specs[j])
-		{
-			if (s[i] == specs[j] && i != 0)
-			{
-				*p = i + 1;
-				return (1);
-			}
-			j++;
-		}
-		j = 0;
-		while (flags[j])
-		{
-			if (s[i] == flags[j])
-				break;
-			j++;
-		}
-		if (i > 0 && j == len_flags)
-			break;
-		i++;
-	}
-	if (!s[i])
+	output = init_buffer();
+	if (output == NULL)
 		return (-1);
-	*p = i;
-	return (0);
-}
 
+	va_start(args, format);
 
-/**
- * _memcpy - copies the memory from src to the buffer
- * @buffer: The destination pointer
- * @src: The source pointer
- * @i_b: Pointer to the index of the buffer
- * @stock: Pointer to the actual stock of bytes written
- * @n: bytes to use from src
- *
- * Description: Copies the memory from src to the buffer inserted.
- * the buffer has a limit of 1024. If the index of buffer is greater than
- * 1024 after the copy, writes the content to the standard output, sum the
- * bytes written and resets the index
- *
- * Return: Nothing(?)
- */
-void _memcpy(char *buffer, const char *src, int *i_b, int *stock, int n)
-{
-	int i = 0;
+	ret = run_printf(format, args, output);
 
-	while (i < n)
-	{
-		*(buffer + *i_b) = *(src + i);
-		i++;
-		*i_b = *i_b + 1;
-		if (*i_b > 1024)
-		{
-			*stock = *stock + *i_b;
-			write(1, buffer, *i_b);
-			*i_b = 0;
-		}
-	}
-}
-
-/**
- * _strlen - return the length of a string
- * @s: Pointer to an string
- *
- * Return: Length of the string
- * If is a null pointer returns 0
- */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	if (s == NULL)
-		return (0);
-
-	while (*(s + i) != '\0')
-		i++;
-
-	return (i);
-}
-
-/**
- * free_buffer - Writes the content of the buffer and frees it
- * @buff: Buffer to free
- * @len: Bytes to write
- *
- * Return: Always -1 (Success)
- */
-int free_buffer(char *buff, int len)
-{
-	write(1, buff, len);
-	free(buff);
-	return (-1);
+	return (ret);
 }
